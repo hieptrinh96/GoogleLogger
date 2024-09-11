c49f51346588ef6e1de551eb62cd85809bf9e0a9
hieptrinh96 <xmikecolx@gmail.com>
2024-09-11 15:12:59 -0500

    Test out image searcher for pyautogui

--- googleLogger.py
+++ googleLogger.py
@@ -4,11 +4,14 @@ import time
 
 len = 1680 / 2
 height = 1050 / 2
+button_location = pyautogui.locateOnScreen('./pictures/testscreenshot.png')
+time.sleep(1)
+pyautogui.click(button_location)
 pyautogui.position()
-print(pyautogui.position())
-pyautogui.moveTo(-1712, 361)
-pyautogui.click()
-pyautogui.click()
-pyautogui.hotkey('command', 'r')
-time.sleep(2)
-pyautogui.hotkey('command', 'ctrl', 'f')
+# print(pyautogui.position())
+# pyautogui.moveTo(-1712, 361)
+# pyautogui.click()
+# pyautogui.click()
+# pyautogui.hotkey('command', 'r')
+# time.sleep(2)
+# pyautogui.hotkey('command', 'ctrl', 'f')
